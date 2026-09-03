1c09753898abf870072de11998695c7df2abe793
Mithun Kannaa <mithunkannaa609@gmail.com>
2026-09-03 10:15:11 +0530

    ns_kernel_3 done, with grouped id mult, shld look to optimize the symmetric matrx mults even further

--- triton_muon.py
+++ triton_muon.py
@@ -161,6 +161,100 @@ def solve_ns_kernel_2(matrix : torch.Tensor, b : float, c:float, out = torch.Ten
     )
 
 @triton.jit
+def ns_kernel_3(a_ptr, 
+        b_ptr,
+        out_ptr,
+        a_coeff,
+        M,
+        N,
+        stride_am,
+        stride_ak,
+        stride_bk,
+        stride_bn,
+        stride_cm ,
+        stride_cn ,
+        BLOCK_M ,
+        BLOCK_N ,
+        BLOCK_K ,
+        GROUP_M ):
+    '''
+    X = (M,N)
+    Compute = aX + out_2 @ X
+    out_2 = bA + cA @ A.T, A = X @ X.T
+    out_2 = (M,M)
+    '''
+    pid = tl.program_id(axis = 0)
+    num_pid_m = tl.cdiv(M,BLOCK_M)
+    num_pid_n = tl.cdiv(N,BLOCK_N)
+    num_pid_in_group = GROUP_M * num_pid_n
+
+    group_id = pid // num_pid_in_group
+    first_pid_m = group_id * GROUP_M
+    group_size_m = tl.minimun(GROUP_M, num_pid_m - first_pid_m)
+
+    pid_m = first_pid_m + pid % group_size_m
+    pid_n = (pid % num_pid_n) // group_size_m
+
+    offset_m = pid_m * BLOCK_M + tl.arange(0,BLOCK_M)
+    offset_n = pid_n * BLOCK_N + tl.arange(0,BLOCK_N)
+    offset_k = tl.arange(0,BLOCK_K)
+
+    a_ptrs = a_ptr + offset_m[:,None] * stride_am + offset_k[None,:] * stride_ak
+    b_ptrs = b_ptr + offset_k[:,None] * stride_bk + offset_n[None,:] * stride_bn
+
+    acc = tl.zeros((BLOCK_M, BLOCK_N), dtype = tl.float32)
+
+    for k in range(0,M,BLOCK_K):
+        mask_k = offset_k + k < M
+
+        a = tl.load(a_ptrs, mask = (offset_m[:,None] < M) & (mask_k[None,:]), other = 0.0)
+        b = tl.load(b_ptrs, mask = (mask_k[:,None]) & (offset_n[None,:] < N), other = 0.0)
+        acc = tl.dot(a,b,acc)
+
+        a_ptrs += BLOCK_K * stride_ak
+        b_ptrs += BLOCK_K * stride_bk
+
+    x_ptrs = a_ptr + offset_m[:,None] * stride_am + offset_n[None,:] * stride_ak
+    mask_x = (offset_m[:,None] < M) & (offset_n[None,:] < N)
+    X = tl.load(x_ptrs, mask = mask_x, other = 0.0)
+
+    out = a_coeff * X + acc 
+    out_ptrs = out_ptr + offset_m[:,None] * stride_cm + offset_n[None,:] * stride_cn
+
+    tl.store(out_ptrs, out, mask = mask_x)
+
+def solve_ns_kernel_3(X : torch.Tensor, out_2 : torch.Tensor, a : float, out = torch.Tensor):
+    M,N = X.shape
+
+    BLOCK_M = 64
+    BLOCK_K = 32
+    BLOCK_N = 64
+    GROUP_M = 8
+
+    grid = ((triton.cdiv(M,BLOCK_M)) * triton.cdiv(N,BLOCK_N))
+
+    ns_kernel_3[grid](
+        a_ptr = out_2,
+        b_ptr = X,
+        out_ptr = out,
+        a_coeff = a,
+        M = M,
+        N = N,
+        stride_am = X.stride(0),
+        stride_ak = X.stride(1),
+        stride_bk = out_2.stride(0),
+        stride_bn = out_2.stride(1),
+        stride_cm = out.stride(0),
+        stride_cn = out.stride(1),
+        BLOCK_M = BLOCK_M,
+        BLOCK_N = BLOCK_N,
+        BLOCK_K = BLOCK_K,
+        GROUP_M = GROUP_M
+    )
+
+
+
+@triton.jit
 def muon_step(): ...
 
 
